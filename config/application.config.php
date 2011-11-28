ca093f76d8738754d47848da6678201dc569afeb
Alexandr <shandy.developer@gmail.com>
2011-11-28 20:44:44 +0200

    Create application and modules skeleton.

---
+++ config/application.config.php
@@ -0,0 +1,16 @@
+<?php
+return array(
+    'module_paths' => array(
+        realpath(dirname(__DIR__) . '/module'),
+        realpath(dirname(__DIR__) . '/vendor'),
+    ),
+    'modules' => array(
+        'Application',
+        'Album'
+    ),
+    'module_listener_options' => array( 
+        'config_cache_enabled'    => false,
+        'cache_dir'               => realpath(dirname(__DIR__) . '/data/cache'),
+        'application_environment' => getenv('APPLICATION_ENV'),
+    ),
+);
